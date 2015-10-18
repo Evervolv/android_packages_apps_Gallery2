6b3e683072149f162218a76c5cbe38829ec77e21
Steve Kondik <steve@cyngn.com>
2015-10-17 23:43:31 -0700

    Gallery2: Revert DRM feature
     * Missing framework support in M, revisit later.
    
    Revert "Gallery2 : Drm file can be set as wallpaper"
    
    This reverts commit b00985f4de70444fa5ece9e8a78a79e73f4475b1.
    
    Revert "Gallery2 : Drm lock icon added on Photo widget for drm content"
    
    This reverts commit 4f3e49619d3954056f899660cc764f3773d92141.
    
    Revert "Gallery2 : GIF animated drm file can be visible in Gallery app"
    
    This reverts commit f5b491b181839286f58d640ae690f1860ef19e8c.
    
    Revert "Gallery2 : Delete and Details options are not visible on Camera preview"
    
    This reverts commit 704bff736bda2bc5f6c5aa2956879f55101191f5.
    
    Revert "Gallery2 : Video share option is not working"
    
    This reverts commit ea87a93df185b9f8c1e477188b55a2fe97dc983e.
    
    Revert "Gallery2 : Uses internal Drm image decoder api in Gallery app"
    
    This reverts commit 01f5f347cf33813b51bd0865dc620d001c8d8735.
    
    Revert "Gallery2 : Added support for Oma drm (CD and SD)"
    
    This reverts commit 43044da1965c1b687695f3f4e1d44d3636496978.
    
    Revert "Gallery2: Make Gallery2 support OMADRM feature"
    
    This reverts commit 829f9ad8c06070038cdef24e5fac76a8a16f841b.
    
    Change-Id: I4300285fd8539eb2e3134a0e472a087b83a0228f

--- src/com/android/gallery3d/app/PhotoPage.java
+++ src/com/android/gallery3d/app/PhotoPage.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (C) 2010 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,7 +24,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
-import android.drm.DrmHelper;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.nfc.NfcAdapter;
@@ -377,9 +376,7 @@ public abstract class PhotoPage extends ActivityState implements
                             }
                             Intent shareIntent = createShareIntent(mCurrentPhoto);
 
-                            if (shareIntent != null) {
-                                mActionBar.setShareIntents(panoramaIntent, shareIntent);
-                            }
+                            mActionBar.setShareIntents(panoramaIntent, shareIntent);
                             setNfcBeamPushUri(contentUri);
                         }
                         break;
@@ -684,7 +681,7 @@ public abstract class PhotoPage extends ActivityState implements
         mNfcPushUris[0] = uri;
     }
 
-    private Intent createShareIntent(MediaObject mediaObject) {
+    private static Intent createShareIntent(MediaObject mediaObject) {
         int type = mediaObject.getMediaType();
         return new Intent(Intent.ACTION_SEND)
                 .setType(MenuExecutor.getMimeType(type))
@@ -803,20 +800,6 @@ public abstract class PhotoPage extends ActivityState implements
             requestDeferredUpdate();
         } else {
             updateUIForCurrentPhoto();
-
-            // Manage DRM rights while image selection changed. this
-            // flow will comes for both image and video, but here
-            // we will consume rights for image files only.
-            // Do not consume rights of a GIF image and video here.
-            // ViewGifImage will take care of GIF rights consumption stub.
-            // MediaPlayer will handle the video rights consumption stub.
-            String mime = mCurrentPhoto.getMimeType();
-            if (!TextUtils.isEmpty(mime) && !mime.equals("image/gif")
-                    && !mime.startsWith("video/")) {
-                DrmHelper.manageDrmLicense(mActivity.getAndroidContext(),
-                        mHandler, mCurrentPhoto.getFilePath(),
-                        mCurrentPhoto.getMimeType());
-            }
         }
     }
 
@@ -1165,12 +1148,6 @@ public abstract class PhotoPage extends ActivityState implements
                 mSelectionManager.toggle(path);
                 mMenuExecutor.onMenuClicked(item, confirmMsg, mConfirmDialogListener);
                 return true;
-            case R.id.action_drm_info:
-                String filepath = current.getFilePath();
-                if (DrmHelper.isDrmFile(filepath)) {
-                    DrmHelper.showDrmInfo(mActivity.getAndroidContext(), filepath);
-                }
-                return true;
             default :
                 return false;
         }
@@ -1437,23 +1414,6 @@ public abstract class PhotoPage extends ActivityState implements
                 UsageStatistics.onContentViewChanged(
                         UsageStatistics.COMPONENT_CAMERA, "Unknown"); // TODO
             }
-
-            // Manage DRM rights while image selection changed. this
-            // flow will comes for both image and video, but here
-            // we will consume rights for image files only.
-            // Do not consume rights of a GIF image and video here.
-            // ViewGifImage will take care of GIF rights consumption stub.
-            // MediaPlayer will handle the video rights consumption stub.
-            if ((mMediaSet != null && mMediaSet.getMediaItemCount() > 1)
-                    || !(this instanceof SinglePhotoPage)) {
-                String mime = mCurrentPhoto.getMimeType();
-                if (!TextUtils.isEmpty(mime) && !mime.equals("image/gif")
-                        && !mime.startsWith("video/")) {
-                    DrmHelper.manageDrmLicense(mActivity.getAndroidContext(),
-                            mHandler, mCurrentPhoto.getFilePath(),
-                            mCurrentPhoto.getMimeType());
-                }
-            }
         }
     }
 
@@ -1626,9 +1586,6 @@ public abstract class PhotoPage extends ActivityState implements
 
     private static void viewAnimateGif(Activity activity, Uri uri) {
         Intent intent = new Intent(ViewGifImage.VIEW_GIF_ACTION, uri);
-        if (DrmHelper.isDrmFile(uri.toString())) {
-            intent.setDataAndType(uri, "image/gif");
-        }
         activity.startActivity(intent);
     }
 }
