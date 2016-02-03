9280bacbf7f530cf3484954a766584bb2a84e84e
Chao Zhang <chaoz@codeaurora.org>
2016-02-03 19:04:35 +0800

    Gallery2: fix server timeout prompt not shown if mStreamingType is HTTP.
    isFullBuffer() will return true if mStreamingType is STREAMING_HTTP.
    
    add STREAMING_HTTP judgement in isFullBuffer().
    
    Change-Id: I520f567f35ed05b735c3a0a5db5cd16e3af75569
    CRs-Fixed: 969149

--- src/com/android/gallery3d/app/MoviePlayer.java
+++ src/com/android/gallery3d/app/MoviePlayer.java
@@ -951,7 +951,8 @@ public class MoviePlayer implements
     }
 
     public boolean isFullBuffer() {
-        if (mStreamingType == STREAMING_RTSP || mStreamingType == STREAMING_SDP) {
+        if (mStreamingType == STREAMING_RTSP || mStreamingType == STREAMING_SDP
+                || mStreamingType == STREAMING_HTTP) {
             return false;
         }
         return true;
