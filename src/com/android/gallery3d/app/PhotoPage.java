f3fa4c0824a74effc41769a2496ff84db516fe21
huiyan <huiyan@codeaurora.org>
2016-05-11 15:10:28 +0800

    Gallery2: Fix force closed when print deleted picture.
    When delete the image file, the permissions are also revoked.
    So securityException occur if read the picture again.
    
    Try to catch the securityException and finish it.
    
    Change-Id: Ic341425ad097cbdec49be8ddd620ee409f632433
    CRs-Fixed: 1004534

--- src/com/android/gallery3d/app/PhotoPage.java
+++ src/com/android/gallery3d/app/PhotoPage.java
@@ -1167,7 +1167,12 @@ public abstract class PhotoPage extends ActivityState implements
                 return true;
             }
             case R.id.print: {
-                mActivity.printSelectedImage(manager.getContentUri(path));
+                try {
+                    mActivity.printSelectedImage(manager.getContentUri(path));
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                    mActivity.finish();
+                }
                 return true;
             }
             case R.id.action_delete:
