codeaurora-unoffical/platform-packages-apps-Gallery2

Gallery2: add some video features
(cherry picked new files from commit
990c6d43ea7c184846e19d41bef0d93aee4e581b)

Change-Id: Ifdcc4e9ab14d1f8b273faa97f0f66ba2f4034bea

src/com/qcom/gallery3d/ext/IContrllerOverlayExt.java
@@ -1,0 +1,51 @@
+package com.qcom.gallery3d.ext;
+/**
+ * Controller overlay extension interface.
+ */
+public interface IContrllerOverlayExt {
+    /**
+     * Show buffering state.
+     * @param fullBuffer
+     * @param percent
+     */
+    void showBuffering(boolean fullBuffer, int percent);
+    /**
+     * Clear buffering state.
+     */
+    void clearBuffering();
+    /**
+     * Show re-connecting state.
+     * @param times
+     */
+    void showReconnecting(int times);
+    /**
+     * Show re-connecting error for connecting fail error.
+     */
+    void showReconnectingError();
+    /**
+     * Show playing info or not.
+     * @param liveStreaming true means showing playing info, otherwise doesn't show playing info.
+     */
+    void setPlayingInfo(boolean liveStreaming);
+    /**
+     * Indicates whether current video can be paused or not.
+     * @param canPause
+     */
+    void setCanPause(boolean canPause);
+    /**
+     * Indicates whether thumb can be scrubbed or not. 
+     * @param enable
+     */
+    void setCanScrubbing(boolean enable);
+    /**
+     * Always show bottmon panel or not.
+     * @param alwaysShow
+     * @param foreShow
+     */
+    void setBottomPanel(boolean alwaysShow, boolean foreShow);
+    /**
+     * Is playing end or not.
+     * @return
+     */
+    boolean isPlayingEnd();
+}

src/com/qcom/gallery3d/ext/IMovieList.java
@@ -1,0 +1,46 @@
+package com.qcom.gallery3d.ext;
+/**
+ * Movie list extension interface
+ */
+public interface IMovieList {
+    /**
+     * Add movie item to list.
+     * @param item
+     */
+    void add(IMovieItem item);
+    /**
+     * Get the item index of list
+     * @param item
+     * @return
+     */
+    int index(IMovieItem item);
+    /**
+     * 
+     * @return list size
+     */
+    int size();
+    /**
+     * 
+     * @param item
+     * @return next item of current item
+     */
+    IMovieItem getNext(IMovieItem item);
+    /**
+     * 
+     * @param item
+     * @return previous item of current item
+     */
+    IMovieItem getPrevious(IMovieItem item);
+    /**
+     * Is first item in list
+     * @param item
+     * @return
+     */
+    boolean isFirst(IMovieItem item);
+    /**
+     * Is last item in list.
+     * @param item
+     * @return
+     */
+    boolean isLast(IMovieItem item);
+}

src/com/qcom/gallery3d/ext/MovieList.java
@@ -1,0 +1,70 @@
+package com.qcom.gallery3d.ext;
+
+import java.util.ArrayList;
+
+public class MovieList implements IMovieList {
+    private static final String TAG = "MovieList";
+    private static final boolean LOG = true;
+    
+    private final ArrayList<IMovieItem> mItems = new ArrayList<IMovieItem>();
+    private static final int UNKNOWN = -1;
+    
+    @Override
+    public void add(IMovieItem item) {
+        if (LOG) {
+            QcomLog.v(TAG, "add(" + item + ")");
+        }
+        mItems.add(item);
+    }
+    
+    @Override
+    public int index(IMovieItem item) {
+        int find = UNKNOWN;
+        int size = mItems.size();
+        for (int i = 0; i < size; i++) {
+            if (item == mItems.get(i)) {
+                find = i;
+                break;
+            }
+        }
+        if (LOG) {
+            QcomLog.v(TAG, "index(" + item + ") return " + find);
+        }
+        return find;
+    }
+    
+    @Override
+    public int size() {
+        return mItems.size();
+    }
+    
+    @Override
+    public IMovieItem getNext(IMovieItem item) {
+        IMovieItem next = null;
+        int find = index(item);
+        if (find >= 0 && find < size() - 1) {
+            next = mItems.get(++find);
+        }
+        return next;
+    }
+    
+    @Override
+    public IMovieItem getPrevious(IMovieItem item) {
+        IMovieItem prev = null;
+        int find = index(item);
+        if (find > 0 && find < size()) {
+            prev = mItems.get(--find);
+        }
+        return prev;
+    }
+
+    @Override
+    public boolean isFirst(IMovieItem item) {
+        return getPrevious(item) == null;
+    }
+
+    @Override
+    public boolean isLast(IMovieItem item) {
+        return getNext(item) == null;
+    }
+}

src/com/qcom/gallery3d/video/MovieListHooker.java
@@ -1,0 +1,116 @@
+package com.qcom.gallery3d.video;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.android.gallery3d.R;
+import com.qcom.gallery3d.ext.IMovieItem;
+import com.qcom.gallery3d.ext.IMovieList;
+import com.qcom.gallery3d.ext.IMovieListLoader;
+import com.qcom.gallery3d.ext.IMovieListLoader.LoaderListener;
+import com.qcom.gallery3d.ext.MovieListLoader;
+import com.qcom.gallery3d.ext.QcomLog;
+
+public class MovieListHooker extends MovieHooker implements LoaderListener {
+    private static final String TAG = "MovieListHooker";
+    private static final boolean LOG = true;
+    
+    private static final int MENU_NEXT = 1;
+    private static final int MENU_PREVIOUS = 2;
+    
+    private MenuItem mMenuNext;
+    private MenuItem mMenuPrevious;
+    
+    private IMovieListLoader mMovieLoader;
+    private IMovieList mMovieList;
+    
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mMovieLoader = new MovieListLoader();
+        mMovieLoader.fillVideoList(getContext(), getIntent(), this, getMovieItem());
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMovieLoader.cancelList();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        if (mMovieList != null) { //list should be filled
+            if (mMovieLoader != null && mMovieLoader.isEnabledVideoList(getIntent())) {
+                mMenuPrevious = menu.add(0, getMenuActivityId(MENU_PREVIOUS), 0, R.string.previous);
+                mMenuNext = menu.add(0, getMenuActivityId(MENU_NEXT), 0, R.string.next);
+            }
+        }
+        return true;
+    }
+    @Override
+    public boolean onPrepareOptionsMenu(final Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        updatePrevNext();
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(getMenuOriginalId(item.getItemId())) {
+        case MENU_PREVIOUS:
+            if (mMovieList == null) {
+                return false;
+            }
+            getPlayer().startNextVideo(mMovieList.getPrevious(getMovieItem()));
+            return true;
+        case MENU_NEXT:
+            if (mMovieList == null) {
+                return false;
+            }
+            getPlayer().startNextVideo(mMovieList.getNext(getMovieItem()));
+            return true;
+        default:
+            return false;
+        }
+    }
+    
+    @Override
+    public void onMovieItemChanged(final IMovieItem item) {
+        super.onMovieItemChanged(item);
+        updatePrevNext();
+    }
+    
+    private void updatePrevNext() {
+        if (LOG) {
+            QcomLog.v(TAG, "updatePrevNext()");
+        }
+        if (mMovieList != null && mMenuPrevious != null && mMenuNext != null) {
+            if (mMovieList.isFirst(getMovieItem()) && mMovieList.isLast(getMovieItem())) { //only one movie
+                mMenuNext.setVisible(false);
+                mMenuPrevious.setVisible(false);
+            } else {
+                mMenuNext.setVisible(true);
+                mMenuPrevious.setVisible(true);
+            }
+            if (mMovieList.isFirst(getMovieItem())) {
+                mMenuPrevious.setEnabled(false);
+            } else {
+                mMenuPrevious.setEnabled(true);
+            }
+            if (mMovieList.isLast(getMovieItem())) {
+                mMenuNext.setEnabled(false);
+            } else {
+                mMenuNext.setEnabled(true);
+            }
+        }
+    }
+    
+    @Override
+    public void onListLoaded(final IMovieList movieList) {
+        mMovieList = movieList;
+        getContext().invalidateOptionsMenu();
+        if (LOG) {
+            QcomLog.v(TAG, "onListLoaded() " + (mMovieList != null ? mMovieList.size() : "null"));
+        }
+    }
+}

src/com/qcom/gallery3d/video/ScreenModeManager.java
@@ -1,0 +1,117 @@
+package com.qcom.gallery3d.video;
+
+import com.qcom.gallery3d.ext.QcomLog;
+
+import java.util.ArrayList;
+
+public class ScreenModeManager {
+    private static final String TAG = "ScreenModeManager";
+    private static final boolean LOG = true;
+    //support screen mode.
+    public static final int SCREENMODE_BIGSCREEN = 1;
+    public static final int SCREENMODE_FULLSCREEN = 2;
+    public static final int SCREENMODE_CROPSCREEN = 4;
+    public static final int SCREENMODE_ALL = 7;
+    
+    private int mScreenMode = SCREENMODE_BIGSCREEN;
+    private int mScreenModes = SCREENMODE_ALL;
+    
+    /**
+     * Enable specified screen mode list. 
+     * The screen mode's value determines the order of being shown. 
+     * <br>you can enable three screen modes by setting screenModes = 
+     * {@link #SCREENMODE_BIGSCREEN} | 
+     * {@link #SCREENMODE_FULLSCREEN} |
+     * {@link #SCREENMODE_CROPSCREEN} or 
+     * just enable two screen modes by setting screenModes = 
+     * {@link #SCREENMODE_BIGSCREEN} | 
+     * {@link #SCREENMODE_CROPSCREEN}.
+     * <br>If current screen mode is the last one of the ordered list, 
+     * then the next screen mode will be the first one of the ordered list.
+     * @param screenModes enabled screen mode list.
+     */
+    public void setScreenModes(final int screenModes) {
+        mScreenModes = (SCREENMODE_BIGSCREEN & screenModes)
+            | (SCREENMODE_FULLSCREEN & screenModes)
+            | (SCREENMODE_CROPSCREEN & screenModes);
+        if ((screenModes & SCREENMODE_ALL) == 0) {
+            mScreenModes = SCREENMODE_ALL;
+            QcomLog.w(TAG, "wrong screenModes=" + screenModes + ". use default value " + SCREENMODE_ALL);
+        }
+        if (LOG) {
+            QcomLog.v(TAG, "enableScreenMode(" + screenModes + ") mScreenModes=" + mScreenModes);
+        }
+    }
+    
+    /**
+     * Get the all screen modes of media controller.
+     * <br><b>Note:</b> it is not the video's current screen mode.
+     * @return the current screen modes.
+     */
+    public int getScreenModes() {
+        return mScreenModes;
+    }
+    
+    public void setScreenMode(final int curScreenMode) {
+        if (LOG) {
+            QcomLog.v(TAG, "setScreenMode(" + curScreenMode + ")");
+        }
+        mScreenMode = curScreenMode;
+        for (final ScreenModeListener listener : mListeners) {
+            listener.onScreenModeChanged(curScreenMode);
+        }
+    }
+    
+    public int getScreenMode() {
+        if (LOG) {
+            QcomLog.v(TAG, "getScreenMode() return " + mScreenMode);
+        }
+        return mScreenMode;
+    }
+    
+    public int getNextScreenMode() {
+        int mode = getScreenMode();
+        mode <<= 1;
+        if ((mode & mScreenModes) == 0) {
+            //not exist, find the right one
+            if (mode > mScreenModes) {
+                mode = 1;
+            }
+            while ((mode & mScreenModes) == 0) {
+                mode <<= 1;
+                if (mode > mScreenModes) {
+                    throw new RuntimeException("wrong screen mode = " + mScreenModes);
+                }
+            }
+        }
+        if (LOG) {
+            QcomLog.v(TAG, "getNextScreenMode() = " + mode);
+        }
+        return mode;
+    }
+    
+    private final ArrayList<ScreenModeListener> mListeners = new ArrayList<ScreenModeListener>();
+    public void addListener(final ScreenModeListener l) {
+        if (!mListeners.contains(l)) {
+            mListeners.add(l);
+        }
+        if (LOG) {
+            QcomLog.v(TAG, "addListener(" + l + ")");
+        }
+    }
+    
+    public void removeListener(final ScreenModeListener l) {
+        mListeners.remove(l);
+        if (LOG) {
+            QcomLog.v(TAG, "removeListener(" + l + ")");
+        }
+    }
+    
+    public void clear() {
+        mListeners.clear();
+    }
+    
+    public interface ScreenModeListener {
+        void onScreenModeChanged(int newMode);
+    }
+}

src/com/qcom/gallery3d/video/StepOptionSettingsHooker.java
@@ -1,0 +1,41 @@
+package com.qcom.gallery3d.video;
+
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.android.gallery3d.R;
+import com.android.gallery3d.app.MovieActivity;
+import com.qcom.gallery3d.ext.ActivityHooker;
+import com.qcom.gallery3d.video.VideoSettingsActivity;
+
+public class StepOptionSettingsHooker extends ActivityHooker {
+    private static final int MENU_STEP_OPTION_SETTING = 1;
+    private MenuItem mMenuStepOption;
+    
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        mMenuStepOption = menu.add(0, getMenuActivityId(MENU_STEP_OPTION_SETTING), 0, R.string.settings);
+        return true;
+    }
+    @Override
+    public boolean onPrepareOptionsMenu(final Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(getMenuOriginalId(item.getItemId())) {
+        case MENU_STEP_OPTION_SETTING:
+            //start activity
+            Intent mIntent = new Intent();
+            mIntent.setClass(getContext(), VideoSettingsActivity.class);
+            getContext().startActivity(mIntent);
+            return true;
+        default:
+            return false;
+        }
+    }
+}

Evervolv/android_packages_apps_Gallery2

(cherry picked files from commit id
including: previous/next video play mode, screen mode,
fast forward/rewind button, step settings.
Change-Id: I3f4890c4dd95956e0eca889a5fd8b8d83b8d542e

src/org/codeaurora/gallery3d/ext/IContrllerOverlayExt.java
@@ -1,0 +1,51 @@
+package org.codeaurora.gallery3d.ext;
+/**
+ * Controller overlay extension interface.
+ */
+public interface IContrllerOverlayExt {
+    /**
+     * Show buffering state.
+     * @param fullBuffer
+     * @param percent
+     */
+    void showBuffering(boolean fullBuffer, int percent);
+    /**
+     * Clear buffering state.
+     */
+    void clearBuffering();
+    /**
+     * Show re-connecting state.
+     * @param times
+     */
+    void showReconnecting(int times);
+    /**
+     * Show re-connecting error for connecting fail error.
+     */
+    void showReconnectingError();
+    /**
+     * Show playing info or not.
+     * @param liveStreaming true means showing playing info, otherwise doesn't show playing info.
+     */
+    void setPlayingInfo(boolean liveStreaming);
+    /**
+     * Indicates whether current video can be paused or not.
+     * @param canPause
+     */
+    void setCanPause(boolean canPause);
+    /**
+     * Indicates whether thumb can be scrubbed or not. 
+     * @param enable
+     */
+    void setCanScrubbing(boolean enable);
+    /**
+     * Always show bottmon panel or not.
+     * @param alwaysShow
+     * @param foreShow
+     */
+    void setBottomPanel(boolean alwaysShow, boolean foreShow);
+    /**
+     * Is playing end or not.
+     * @return
+     */
+    boolean isPlayingEnd();
+}

src/org/codeaurora/gallery3d/ext/IMovieList.java
@@ -1,0 +1,46 @@
+package org.codeaurora.gallery3d.ext;
+/**
+ * Movie list extension interface
+ */
+public interface IMovieList {
+    /**
+     * Add movie item to list.
+     * @param item
+     */
+    void add(IMovieItem item);
+    /**
+     * Get the item index of list
+     * @param item
+     * @return
+     */
+    int index(IMovieItem item);
+    /**
+     * 
+     * @return list size
+     */
+    int size();
+    /**
+     * 
+     * @param item
+     * @return next item of current item
+     */
+    IMovieItem getNext(IMovieItem item);
+    /**
+     * 
+     * @param item
+     * @return previous item of current item
+     */
+    IMovieItem getPrevious(IMovieItem item);
+    /**
+     * Is first item in list
+     * @param item
+     * @return
+     */
+    boolean isFirst(IMovieItem item);
+    /**
+     * Is last item in list.
+     * @param item
+     * @return
+     */
+    boolean isLast(IMovieItem item);
+}

src/org/codeaurora/gallery3d/ext/MovieList.java
@@ -1,0 +1,72 @@
+package org.codeaurora.gallery3d.ext;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class MovieList implements IMovieList {
+    private static final String TAG = "MovieList";
+    private static final boolean LOG = true;
+    
+    private final ArrayList<IMovieItem> mItems = new ArrayList<IMovieItem>();
+    private static final int UNKNOWN = -1;
+    
+    @Override
+    public void add(IMovieItem item) {
+        if (LOG) {
+            Log.v(TAG, "add(" + item + ")");
+        }
+        mItems.add(item);
+    }
+    
+    @Override
+    public int index(IMovieItem item) {
+        int find = UNKNOWN;
+        int size = mItems.size();
+        for (int i = 0; i < size; i++) {
+            if (item == mItems.get(i)) {
+                find = i;
+                break;
+            }
+        }
+        if (LOG) {
+            Log.v(TAG, "index(" + item + ") return " + find);
+        }
+        return find;
+    }
+    
+    @Override
+    public int size() {
+        return mItems.size();
+    }
+    
+    @Override
+    public IMovieItem getNext(IMovieItem item) {
+        IMovieItem next = null;
+        int find = index(item);
+        if (find >= 0 && find < size() - 1) {
+            next = mItems.get(++find);
+        }
+        return next;
+    }
+    
+    @Override
+    public IMovieItem getPrevious(IMovieItem item) {
+        IMovieItem prev = null;
+        int find = index(item);
+        if (find > 0 && find < size()) {
+            prev = mItems.get(--find);
+        }
+        return prev;
+    }
+
+    @Override
+    public boolean isFirst(IMovieItem item) {
+        return getPrevious(item) == null;
+    }
+
+    @Override
+    public boolean isLast(IMovieItem item) {
+        return getNext(item) == null;
+    }
+}

src/org/codeaurora/gallery3d/video/MovieListHooker.java
@@ -1,0 +1,116 @@
+package org.codeaurora.gallery3d.video;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.android.gallery3d.R;
+import org.codeaurora.gallery3d.ext.IMovieItem;
+import org.codeaurora.gallery3d.ext.IMovieList;
+import org.codeaurora.gallery3d.ext.IMovieListLoader;
+import org.codeaurora.gallery3d.ext.IMovieListLoader.LoaderListener;
+import org.codeaurora.gallery3d.ext.MovieListLoader;
+
+public class MovieListHooker extends MovieHooker implements LoaderListener {
+    private static final String TAG = "MovieListHooker";
+    private static final boolean LOG = true;
+    
+    private static final int MENU_NEXT = 1;
+    private static final int MENU_PREVIOUS = 2;
+    
+    private MenuItem mMenuNext;
+    private MenuItem mMenuPrevious;
+    
+    private IMovieListLoader mMovieLoader;
+    private IMovieList mMovieList;
+    
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mMovieLoader = new MovieListLoader();
+        mMovieLoader.fillVideoList(getContext(), getIntent(), this, getMovieItem());
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMovieLoader.cancelList();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        if (mMovieList != null) { //list should be filled
+            if (mMovieLoader != null && mMovieLoader.isEnabledVideoList(getIntent())) {
+                mMenuPrevious = menu.add(0, getMenuActivityId(MENU_PREVIOUS), 0, R.string.previous);
+                mMenuNext = menu.add(0, getMenuActivityId(MENU_NEXT), 0, R.string.next);
+            }
+        }
+        return true;
+    }
+    @Override
+    public boolean onPrepareOptionsMenu(final Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        updatePrevNext();
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(getMenuOriginalId(item.getItemId())) {
+        case MENU_PREVIOUS:
+            if (mMovieList == null) {
+                return false;
+            }
+            getPlayer().startNextVideo(mMovieList.getPrevious(getMovieItem()));
+            return true;
+        case MENU_NEXT:
+            if (mMovieList == null) {
+                return false;
+            }
+            getPlayer().startNextVideo(mMovieList.getNext(getMovieItem()));
+            return true;
+        default:
+            return false;
+        }
+    }
+    
+    @Override
+    public void onMovieItemChanged(final IMovieItem item) {
+        super.onMovieItemChanged(item);
+        updatePrevNext();
+    }
+    
+    private void updatePrevNext() {
+        if (LOG) {
+            Log.v(TAG, "updatePrevNext()");
+        }
+        if (mMovieList != null && mMenuPrevious != null && mMenuNext != null) {
+            if (mMovieList.isFirst(getMovieItem()) && mMovieList.isLast(getMovieItem())) { //only one movie
+                mMenuNext.setVisible(false);
+                mMenuPrevious.setVisible(false);
+            } else {
+                mMenuNext.setVisible(true);
+                mMenuPrevious.setVisible(true);
+            }
+            if (mMovieList.isFirst(getMovieItem())) {
+                mMenuPrevious.setEnabled(false);
+            } else {
+                mMenuPrevious.setEnabled(true);
+            }
+            if (mMovieList.isLast(getMovieItem())) {
+                mMenuNext.setEnabled(false);
+            } else {
+                mMenuNext.setEnabled(true);
+            }
+        }
+    }
+    
+    @Override
+    public void onListLoaded(final IMovieList movieList) {
+        mMovieList = movieList;
+        getContext().invalidateOptionsMenu();
+        if (LOG) {
+            Log.v(TAG, "onListLoaded() " + (mMovieList != null ? mMovieList.size() : "null"));
+        }
+    }
+}

src/org/codeaurora/gallery3d/video/ScreenModeManager.java
@@ -1,0 +1,117 @@
+package org.codeaurora.gallery3d.video;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class ScreenModeManager {
+    private static final String TAG = "ScreenModeManager";
+    private static final boolean LOG = true;
+    //support screen mode.
+    public static final int SCREENMODE_BIGSCREEN = 1;
+    public static final int SCREENMODE_FULLSCREEN = 2;
+    public static final int SCREENMODE_CROPSCREEN = 4;
+    public static final int SCREENMODE_ALL = 7;
+    
+    private int mScreenMode = SCREENMODE_BIGSCREEN;
+    private int mScreenModes = SCREENMODE_ALL;
+    
+    /**
+     * Enable specified screen mode list. 
+     * The screen mode's value determines the order of being shown. 
+     * <br>you can enable three screen modes by setting screenModes = 
+     * {@link #SCREENMODE_BIGSCREEN} | 
+     * {@link #SCREENMODE_FULLSCREEN} |
+     * {@link #SCREENMODE_CROPSCREEN} or 
+     * just enable two screen modes by setting screenModes = 
+     * {@link #SCREENMODE_BIGSCREEN} | 
+     * {@link #SCREENMODE_CROPSCREEN}.
+     * <br>If current screen mode is the last one of the ordered list, 
+     * then the next screen mode will be the first one of the ordered list.
+     * @param screenModes enabled screen mode list.
+     */
+    public void setScreenModes(final int screenModes) {
+        mScreenModes = (SCREENMODE_BIGSCREEN & screenModes)
+            | (SCREENMODE_FULLSCREEN & screenModes)
+            | (SCREENMODE_CROPSCREEN & screenModes);
+        if ((screenModes & SCREENMODE_ALL) == 0) {
+            mScreenModes = SCREENMODE_ALL;
+            Log.w(TAG, "wrong screenModes=" + screenModes + ". use default value " + SCREENMODE_ALL);
+        }
+        if (LOG) {
+            Log.v(TAG, "enableScreenMode(" + screenModes + ") mScreenModes=" + mScreenModes);
+        }
+    }
+    
+    /**
+     * Get the all screen modes of media controller.
+     * <br><b>Note:</b> it is not the video's current screen mode.
+     * @return the current screen modes.
+     */
+    public int getScreenModes() {
+        return mScreenModes;
+    }
+    
+    public void setScreenMode(final int curScreenMode) {
+        if (LOG) {
+            Log.v(TAG, "setScreenMode(" + curScreenMode + ")");
+        }
+        mScreenMode = curScreenMode;
+        for (final ScreenModeListener listener : mListeners) {
+            listener.onScreenModeChanged(curScreenMode);
+        }
+    }
+    
+    public int getScreenMode() {
+        if (LOG) {
+            Log.v(TAG, "getScreenMode() return " + mScreenMode);
+        }
+        return mScreenMode;
+    }
+    
+    public int getNextScreenMode() {
+        int mode = getScreenMode();
+        mode <<= 1;
+        if ((mode & mScreenModes) == 0) {
+            //not exist, find the right one
+            if (mode > mScreenModes) {
+                mode = 1;
+            }
+            while ((mode & mScreenModes) == 0) {
+                mode <<= 1;
+                if (mode > mScreenModes) {
+                    throw new RuntimeException("wrong screen mode = " + mScreenModes);
+                }
+            }
+        }
+        if (LOG) {
+            Log.v(TAG, "getNextScreenMode() = " + mode);
+        }
+        return mode;
+    }
+    
+    private final ArrayList<ScreenModeListener> mListeners = new ArrayList<ScreenModeListener>();
+    public void addListener(final ScreenModeListener l) {
+        if (!mListeners.contains(l)) {
+            mListeners.add(l);
+        }
+        if (LOG) {
+            Log.v(TAG, "addListener(" + l + ")");
+        }
+    }
+    
+    public void removeListener(final ScreenModeListener l) {
+        mListeners.remove(l);
+        if (LOG) {
+            Log.v(TAG, "removeListener(" + l + ")");
+        }
+    }
+    
+    public void clear() {
+        mListeners.clear();
+    }
+    
+    public interface ScreenModeListener {
+        void onScreenModeChanged(int newMode);
+    }
+}

src/org/codeaurora/gallery3d/video/StepOptionSettingsHooker.java
@@ -1,0 +1,41 @@
+package org.codeaurora.gallery3d.video;
+
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.android.gallery3d.R;
+import com.android.gallery3d.app.MovieActivity;
+import org.codeaurora.gallery3d.ext.ActivityHooker;
+import org.codeaurora.gallery3d.video.VideoSettingsActivity;
+
+public class StepOptionSettingsHooker extends ActivityHooker {
+    private static final int MENU_STEP_OPTION_SETTING = 1;
+    private MenuItem mMenuStepOption;
+    
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        mMenuStepOption = menu.add(0, getMenuActivityId(MENU_STEP_OPTION_SETTING), 0, R.string.settings);
+        return true;
+    }
+    @Override
+    public boolean onPrepareOptionsMenu(final Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(getMenuOriginalId(item.getItemId())) {
+        case MENU_STEP_OPTION_SETTING:
+            //start activity
+            Intent mIntent = new Intent();
+            mIntent.setClass(getContext(), VideoSettingsActivity.class);
+            getContext().startActivity(mIntent);
+            return true;
+        default:
+            return false;
+        }
+    }
+}