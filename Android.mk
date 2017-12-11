c023bfb953b11059a5a4a506da270f40e86bcc39
Alan Viverette <alanv@google.com>
2017-12-11 17:57:32 -0500

    Move Gallery2 over to AAPT2 and new v4 modules
    Bug: 35994894
    Test: make
    Change-Id: I00cacda9dc8dc9ac919958cc7a8bd432680b85d7

--- Android.mk
+++ Android.mk
@@ -4,29 +4,35 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_JAVA_LIBRARIES := telephony-common
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    android-support-design \
+    android-support-fragment \
+    android-support-core-ui \
+    android-support-compat \
+    android-support-transition \
+    android-support-v7-appcompat \
+    android-support-v7-recyclerview \
+    android-support-v13
 
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-design
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-transition
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v13
-LOCAL_STATIC_JAVA_LIBRARIES += org.codeaurora.gallery.common
-LOCAL_STATIC_JAVA_LIBRARIES += mp4parser
-LOCAL_STATIC_JAVA_LIBRARIES += xmp_toolkit
-LOCAL_STATIC_JAVA_LIBRARIES += androidx.heifwriter_heifwriter
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    androidx.heifwriter_heifwriter \
+    org.codeaurora.gallery.common \
+    xmp_toolkit \
+    mp4parser
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-renderscript-files-under, src)
-LOCAL_SRC_FILES += $(call all-java-files-under, src_pd)
+LOCAL_JAVA_LIBRARIES := \
+    telephony-common \
+    org.apache.http.legacy
+
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, src) \
+    $(call all-renderscript-files-under, src) \
+    $(call all-java-files-under, src_pd)
 
 LOCAL_RESOURCE_DIR += \
-    $(LOCAL_PATH)/res \
-    $(TOP)/frameworks/support/design/res \
-    $(TOP)/frameworks/support/v7/appcompat/res
+    $(LOCAL_PATH)/res
 
-LOCAL_AAPT_FLAGS := \
-    --auto-add-overlay \
-    --extra-packages android.support.design \
-    --extra-packages android.support.v7.appcompat
+LOCAL_USE_AAPT2 := true
 
 LOCAL_PACKAGE_NAME := Gallery2
 LOCAL_PRIVILEGED_MODULE := true
@@ -36,19 +42,21 @@ LOCAL_PRIVATE_PLATFORM_APIS:=true
 
 LOCAL_OVERRIDES_PACKAGES := Gallery Gallery3D GalleryNew3D
 
-LOCAL_AAPT_FLAGS += --rename-manifest-package com.android.gallery3d
+LOCAL_AAPT_FLAGS := --rename-manifest-package com.android.gallery3d
 
 #LOCAL_SDK_VERSION := current
 LOCAL_MIN_SDK_VERSION := 23
 
-LOCAL_JNI_SHARED_LIBRARIES := libjni_gallery_eglfence libjni_gallery_filters libjni_gallery_jpegstream
+LOCAL_JNI_SHARED_LIBRARIES := \
+    libjni_gallery_eglfence \
+    libjni_gallery_filters \
+    libjni_gallery_jpegstream
+
 LOCAL_SHARED_LIBRARIES += libjni_dualcamera libjni_trueportrait libjni_filtergenerator
 
 LOCAL_REQUIRED_MODULES := libts_detected_face_jni libts_face_beautify_jni
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
-
-LOCAL_JAVA_LIBRARIES += org.apache.http.legacy
 
 include $(BUILD_PACKAGE)
 
